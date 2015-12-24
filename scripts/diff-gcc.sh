bedf9859ad8515aa1b7dda3d502e8b44f9352d74
alpine9000 <alex.g.brown@gmail.com>
2015-12-25 09:37:33 +1100

    Scripts to patch ports

---
+++ scripts/diff-gcc.sh
@@ -0,0 +1,2 @@
+cd ~/Projects/bitos-build/src/gcc-5.3.0
+svn diff > ~/Projects/BitOS/patches/gcc-5.3.0.patch
---
+++ scripts/diff-make.sh
@@ -0,0 +1,2 @@
+cd ~/Projects/bitos-build/src
+diff -Naur make-4.1.orig make-4.1.bitos  > ~/Projects/BitOS/patches/make-4.1.patch
